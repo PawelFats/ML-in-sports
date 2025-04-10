14fe41cb7241c957b90987af04690e845b9448a6
PawelFats <pawelfats@gmail.com>
2025-04-10 23:19:20 +0700

    added pages for bayesian method

--- app/ui/ui_pages/__init__.py
+++ app/ui/ui_pages/__init__.py
@@ -4,3 +4,4 @@ from .rankings import rankings
 from .charts import charts
 from .player_rt_intg import player_rt_intg
 from .player_rt_red import player_rt_red
+from .bayesian_logic import bayesian_analysis
