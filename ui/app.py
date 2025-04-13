54d304305e55f303ef46d917b66e59d43ceddc91
PawelFats <pawelfats@gmail.com>
2025-04-13 17:55:47 +0700

    fix

--- ui/app.py
+++ ui/app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 import os
-print("Current working dir:", os.getcwd())
+import sys
+
+# Определяем корневую папку проекта: берем родительскую директорию от текущей директории файла
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if project_root not in sys.path:
+    sys.path.insert(0, project_root)
 
 from ui import ui_pages
 
